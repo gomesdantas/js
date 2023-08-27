5f8ca366144c0875d79c094188dd127d91ddd22b
gomesdantas <biancagomesdantas@gmail.com>
2023-08-27 18:52:17 -0300

    first commit

---
+++ programacaodois/projeto.js
@@ -0,0 +1,10 @@
+import prompt from 'prompt-sync'
+let ler = prompt();
+
+console.log('Qual seu nome?');
+let nome=ler();
+
+console.log ('Boas vindas ' + nome);
+console.log('quantos anos você tem?')
+let anos=ler();
+
---
+++ teste/teste.js
@@ -0,0 +1,8 @@
+import prompt from 'prompt-sync';
+let ler = prompt();
+
+
+console.log('qual seu nome ?');
+let nome=ler();
+
+console.log('Prazer ' + nome);
